0d6d8a9ad7a92a9e316e83624256d24178804d06
Jon <JTP709@gmail.com>
2018-06-05 14:22:12 -0400

    fancier tests

--- sets_test.py
+++ sets_test.py
@@ -1,39 +1,41 @@
 import unittest
 from sets import Set, union, intersection, setDifference, relativeCompliment, symmetricDifference
 
+def addHelper(data):
+	testSet = Set()
+	for i in data:
+		testSet.add(i)
+	return testSet
+
+def testHasHelper(testObject, testList):
+	for i in testList:
+		if testObject.has(i) == False:
+			return False
+	return True
+
+
 class SetTests(unittest.TestCase):
 	def test_add_to_set(self):
-		testSet = Set()
-		testSet.add(1)
-		testSet.add('B')
-		testSet.add('C')
-		self.assertTrue(testSet.has(1))
-		self.assertTrue(testSet.has('B'))
-		self.assertTrue(testSet.has('C'))
+		testData = (1,'B','C')
+		testSet = addHelper(testData)
+
+		self.assertTrue(testHasHelper(testSet, testData))
 		self.assertFalse(testSet.has('D'))
 
 	def test_remove_from_set(self):
-		testSet = Set()
-		testSet.add('A')
-		testSet.add('B')
-		testSet.add('C')
-		self.assertTrue(testSet.has('A'))
-
+		testData = ('A','B','C')
+		testSet = addHelper(testData)
 		testSet.remove('A')
+
 		self.assertFalse(testSet.has('A'))
 
 	def test_union(self):
-		testSetUno = Set()
-		testSetUno.add('A')
-		testSetUno.add('B')
-		testSetUno.add('C')
-
-		testSetDos = Set()
-		testSetDos.add('D')
-		testSetDos.add('E')
-		testSetDos.add('F')
-
+		testData1 = ('A','B','C')
+		testSetUno = addHelper(testData1)
+		testData2 = ('D','E','F')
+		testSetDos = addHelper(testData2)
 		testUnion = union(testSetUno.getSet(), testSetDos.getSet())
+
 		self.assertTrue('A' in testUnion.values())
 		self.assertTrue('B' in testUnion.values())
 		self.assertTrue('C' in testUnion.values())
@@ -42,69 +44,45 @@ class SetTests(unittest.TestCase):
 		self.assertTrue('F' in testUnion.values())
 
 	def test_intersection(self):
-		testSetUno = Set()
-		testSetUno.add('A')
-		testSetUno.add('B')
-		testSetUno.add('C')
-
-		testSetDos = Set()
-		testSetDos.add('A')
-		testSetDos.add('B')
-		testSetDos.add('F')
-
+		testData1 = ('A','B','C')
+		testSetUno = addHelper(testData1)
+		testData2 = ('A','B','F')
+		testSetDos = addHelper(testData2)
 		testIntersection = intersection(testSetUno.getSet(), testSetDos.getSet())
+
 		self.assertTrue('A' in testIntersection.values())
 		self.assertTrue('B' in testIntersection.values())
 		self.assertFalse('C' in testIntersection.values())
 
 	def test_setDifference(self):
-		testSetUno = Set()
-		testSetUno.add('A')
-		testSetUno.add('B')
-		testSetUno.add('C')
-		testSetUno.add('D')
-
-		testSetDos = Set()
-		testSetDos.add('A')
-		testSetDos.add('B')
-		testSetDos.add('F')
-
+		testData1 = ('A','B','C','D')
+		testSetUno = addHelper(testData1)
+		testData2 = ('A','B','F')
+		testSetDos = addHelper(testData2)
 		testIntersection = setDifference(testSetUno.getSet(), testSetDos.getSet())
+
 		self.assertTrue('C' in testIntersection.values())
 		self.assertTrue('D' in testIntersection.values())
 		self.assertFalse('A' in testIntersection.values())
 
 	def test_relativeCompliment(self):
-		testSetUno = Set()
-		testSetUno.add('A')
-		testSetUno.add('B')
-		testSetUno.add('E')
-
-		testSetDos = Set()
-		testSetDos.add('A')
-		testSetDos.add('B')
-		testSetDos.add('C')
-		testSetDos.add('D')
-
+		testData1 = ('A','B','E')
+		testSetUno = addHelper(testData1)
+		testData2 = ('A','B','C','D')
+		testSetDos = addHelper(testData2)
 		testRelativeCompliment = relativeCompliment(testSetUno.getSet(), testSetDos.getSet())
+
 		self.assertTrue('C' in testRelativeCompliment.values())
 		self.assertTrue('D' in testRelativeCompliment.values())
 		self.assertFalse('A' in testRelativeCompliment.values())
 
 	def test_symmetricDifference(self):
-		testSetUno = Set()
-		testSetUno.add('A')
-		testSetUno.add('B')
-		testSetUno.add('E')
-		testSetUno.add('F')
-
-		testSetDos = Set()
-		testSetDos.add('A')
-		testSetDos.add('B')
-		testSetDos.add('C')
-		testSetDos.add('D')
-
+		testData1 = ('A','B','E','F')
+		testSetUno = addHelper(testData1)
+		testData2 = ('A','B','C','D')
+		testSetDos = addHelper(testData2)
 		testSymmetricDifference = symmetricDifference(testSetUno.getSet(), testSetDos.getSet())
+
 		self.assertTrue('E' in testSymmetricDifference.values())
 		self.assertTrue('F' in testSymmetricDifference.values())
 		self.assertTrue('C' in testSymmetricDifference.values())
